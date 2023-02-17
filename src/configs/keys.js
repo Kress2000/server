a8be0f11533d9e62c64389f3299e4b5c0a17938b
nsanzimfura eric <erickykress1@gmail.com>
2023-02-17 15:29:18 +0200

    connection

--- src/configs/keys.js
+++ src/configs/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoUri : 'mongodb+srv://my-brand:kress123@cluster0.638azsu.mongodb.net/my-brand'
+    mongoUri : process.env.DB_URI
+    // 'mongodb+srv://my-brand:kress123@cluster0.638azsu.mongodb.net/my-brand'
 }
